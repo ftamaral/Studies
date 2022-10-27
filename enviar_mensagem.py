0a496a04707493ed2b4492c47e41023709562ab9
ftamaral <114092215+ftamaral@users.noreply.github.com>
2022-10-27 16:47:51 -0300

    Update enviar_mensagem.py

--- enviar_mensagem.py
+++ enviar_mensagem.py
@@ -1,3 +1,5 @@
+# Está automção envia uma mensagem via Teams para usuários listado em uma planilha excel.
+
 #--------- BIBLIOTECAS ----------------------
 
 import pyautogui
@@ -10,22 +12,31 @@ from pynput.keyboard import Controller
 
 keyboard = Controller()
 df = pd.read_excel('C:\\planilha_excel.xlsx')
-bd = 'Olá, bom dia\n'
 teste_auto = 'É uma mensagem de teste, não precisa ser respondida.\n'
+hatual = datetime.now()
+heditada = float(hatual.strftime("%H"))
+meio_dia = float(12)
 
 #--------- AUTOMAÇÃO -------------------------
 
-for u in df["Usuario"]:
-    sleep(2)
-    pyautogui.moveTo(x=560, y=24)
-    sleep(2)
-    pyautogui.leftClick(x=560, y=24)
-    keyboard.type(u + "@dominio.com.br")
-    sleep(2)
+for a in df["Usuario"]:                             # Loop
+    
+    sleep(1)
+    pyautogui.moveTo(x=560, y=34)
+    sleep(1)
+    pyautogui.leftClick(x=560, y=34)
+    sleep(1)
+    keyboard.type(a + "@sicredi.com.br")
+    sleep(3)
     pyautogui.leftClick(x=484, y=147)
-    sleep(2)
+    sleep(3)
     pyautogui.moveTo(x=663, y=835)
-    sleep(2)
+    sleep(3)
     pyautogui.leftClick(x=663, y=835)
-    sleep(2)
-    keyboard.type(bd)
+    sleep(3)
+    if heditada < meio_dia:                         # Condicional
+        keyboard.type('Olá bom dia\n')
+    else:
+        keyboard.type('Olá boa tarde\n')
+    sleep(3)
+    keyboard.type(msn)
